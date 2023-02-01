736cc6944b2ecc4c7c858272bf1a4d1decdf39e6
HeonSeokCha <hs960829@gmail.com>
2023-02-01 23:17:46 +0900

    add related Tag List.

--- app/src/main/java/com/chs/yoursplash/presentation/browse/photo_detail/PhotoDetailInfo.kt
+++ app/src/main/java/com/chs/yoursplash/presentation/browse/photo_detail/PhotoDetailInfo.kt
@@ -1,15 +1,16 @@
 package com.chs.yoursplash.presentation.browse.photo_detail
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.chs.yoursplash.domain.model.PhotoDetail
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun ImageDetailInfo(
     imageDetailInfo: PhotoDetail?
@@ -57,5 +58,25 @@ fun ImageDetailInfo(
             }
         }
         Divider(modifier = Modifier.padding(top = 16.dp, bottom = 16.dp))
+
+        Text(
+            text = "Related tags",
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        FlowRow {
+            imageDetailInfo?.tags?.filter { it.type == "search" }?.forEach { tag ->
+               Chip(
+                  onClick = {},
+                  colors = ChipDefaults.chipColors(
+                      backgroundColor = Color.LightGray,
+                      contentColor = Color.Black
+                  )
+               ) {
+                   Text(tag.title)
+               }
+            }
+        }
     }
 }
